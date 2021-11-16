38c038663879ed13e0305786c04d7733e91f5711
Sami Salih İbrahimbaş <sami.salih@windowslive.com>
2021-11-16 20:34:44 +0300

    autoFocus added

---
+++ tips/src/examples/autoFocus/autoFocus.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="form-group">
+    <input class="form-control" ref="message" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class AutoFocus extends Vue {
+  declare $refs: {
+    message: HTMLInputElement;
+  };
+
+  focusInput(): void {
+    this.$refs.message.focus();
+  }
+
+  mounted(): void {
+    this.focusInput();
+  }
+}
+</script>
